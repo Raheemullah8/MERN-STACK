88a3f92c9a71eb6ac6adf0c6d9ee01b112819b61
Kashif0955 <154120701+Raheemullah8@users.noreply.github.com>
2025-03-03 10:27:26 -0800

    Crud-system perform using nodejs express mongodb ejs

--- backend/app.js
+++ backend/app.js
@@ -12,9 +12,29 @@ app.use(express.urlencoded({extended:true}));
 app.get('/',(req,res)=>{
     res.render("index");
 });
-app.post('/registeruser',(req,res)=>{
-    console.log(req.body);
-    res.send("Data received");
+app.post('/registeruser', async(req,res)=>{
+    const {name,email,password} = req.body;
+    const user = await userModel.create({
+        name,
+        email,
+        password
+    });
+
+    res.send(user);
+})
+app.get('/getUser', async (req,res)=>{
+ const userData = await userModel.find({});
+ res.send(userData);
+})
+app.get('/updateUser',async(req,res)=>{
+    
+    const updateuser = await userModel.findOneAndUpdate({name:"b"},{email:"c@c.com"})
+    res.send("updateUser");
+})
+app.get('/deleteUser',async(req,res)=>{
+    const deleteuser = await userModel.findOneAndDelete({"email":"c@c.com"})
+
+    res.send(deleteuser);
 })
 
 app.listen(port,()=>{
